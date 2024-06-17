d2f9c92313d844be87206fdf397e92d9b152fd2f
Desarrollo-frontend <desarrollo.frontend@robotica.com.co>
2024-06-17 17:04:37 -0500

    Update main.py
    v4

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import pyodbc
-import os
 
 app = Flask(__name__)
 
-# Configuración de la base de datos utilizando variables de entorno
+# Configuración de la base de datos
 DATABASE_CONFIG = {
-    'server': os.getenv('DB_SERVER', '191.239.122.127'),
-    'database': os.getenv('DB_NAME', 'FNA'),
-    'username': os.getenv('DB_USERNAME', 'FNA'),
-    'password': os.getenv('DB_PASSWORD', 'D0cUm3nT2024*.'),
+    'server': "191.239.122.127",  # Dirección IP del servidor SQL
+    'database': "FNA",
+    'username': "FNA",
+    'password': "D0cUm3nT2024*.",
     'driver': '{SQL Server}'
 }
 
 def connection():
     # Establece los parámetros de conexión a la base de datos SQL Server
     conn_str = f"DRIVER={DATABASE_CONFIG['driver']};SERVER={DATABASE_CONFIG['server']};DATABASE={DATABASE_CONFIG['database']};UID={DATABASE_CONFIG['username']};PWD={DATABASE_CONFIG['password']}"
-
+    
     # Conecta a la base de datos
     connectionsql = pyodbc.connect(conn_str)
-
+    
     return connectionsql
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/data/<cedula>')
 def getByCedula(cedula):
@@ -29,9 +32,9 @@ def getByCedula(cedula):
         connectionql = connection()
         cursor = connectionql.cursor()
 
-        # Ejecuta la consulta SQL parametrizada para obtener los datos
-        query = "SELECT inv_number, d_asunto, d_number FROM inventoryview WHERE d_number LIKE ?"
-        cursor.execute(query, ('%' + cedula + '%',))
+        # Ejecuta la consulta SQL para obtener los datos
+        query = f"SELECT inv_number, d_asunto, d_number FROM inventoryview WHERE d_number LIKE '%{cedula}%'"
+        cursor.execute(query)
 
         # Obtiene los resultados de la consulta
         data = cursor.fetchall()
@@ -43,8 +46,13 @@ def getByCedula(cedula):
         cursor.close()
         connectionql.close()
 
-        # Devuelve los datos en formato JSON
-        return jsonify(result)
+        # Verifica el tipo de solicitud: JSON o texto plano
+        if 'application/json' in request.headers.get('Accept', ''):
+            return jsonify(result)
+        else:
+            # Si la solicitud no acepta JSON, devolver texto plano
+            result_text = '\n'.join([f"Inv Number: {row['inv_number']}, Asunto: {row['d_asunto']}, D Number: {row['d_number']}" for row in result])
+            return result_text
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
